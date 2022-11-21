b8a42dc10bba646f4d4ecef64163f01ae87d617a
Ignacio <ignaciogpovina@gmail.com>
2022-11-21 15:18:16 -0500

    arregladas las notificaciones con filtro externo

--- docdeck/DashboardApp/templatetags/dashboard_extras.py
+++ docdeck/DashboardApp/templatetags/dashboard_extras.py
@@ -5,6 +5,4 @@ register = template.Library()
 
 @register.filter
 def read(nuevas_consultas):
-    print('hola')
-    print(Consulta.objects.filter(read=False))
     return Consulta.objects.filter(read=False)
